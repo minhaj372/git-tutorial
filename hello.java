cb01b7204b8b99110607047ccec0c476b544fa0e
minhaj372 <minhaj372@gmail.com>
2020-06-04 15:41:46 +0600

    all remove and add message nethood

--- hello.java
+++ hello.java
@@ -1,19 +1,9 @@
 public class hello{
     public static void main(string[] args) {
-        system.out.println("vs are awesome..");
-        greeting ("greeting")
-        call ("greeting")
+
     }
-    public static void greeting(string name) {
-        
-    }
-    public static void greeting(string name) {
-        system.out.println("Hello" . name);
-    }
-    public static void greeting(string name) {
-        system.out.println("hello hello hello hello" . name);
-    public static void call(string call) {
-        
+    public static void message() {
+        system.out.println("Horray!!! now im contribute on open source...")
     }
 
 
