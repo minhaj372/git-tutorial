545235723dd1b6476c6510b991f9f27f791c3d6c
minhaj372 <minhaj372@gmail.com>
2020-06-04 14:52:21 +0600

    added some line for learning

--- hello.java
+++ hello.java
@@ -2,8 +2,12 @@ public class hello{
     public static void main(string[] args) {
         system.out.println("vs are awesome..");
         greeting ("greeting")
+        call ("greeting")
     }
     public static void greeting(string name) {
+        
+    }
+    public static void call(string call) {
         
     }
 
