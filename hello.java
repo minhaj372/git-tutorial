a73836ec7aeaffe1a1fe63476ae5a04e0b37481a
minhaj372 <minhaj372@gmail.com>
2020-06-04 13:16:04 +0600

    change greeting in hello.java

--- hello.java
+++ hello.java
@@ -1,10 +1,10 @@
 public class hello{
     public static void main(string[] args) {
         system.out.println("vs are awesome..");
-        greeting ("MD MINHAJ")
+        greeting ("greeting")
     }
     public static void greeting(string name) {
-        system.out.println("welcome" . name);
+        
     }
 
 
